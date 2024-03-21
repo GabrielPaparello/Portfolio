bad0d7bafe754e1f694bc011baa320092de3ab1a
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-03-21 16:09:17 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+function addElement() {
+    // create a new div element
+    const newDiv = document.createElement("div");
+  
+    // and give it some content
+    const newContent = document.createTextNode("1");
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+  
+    // add the newly created element and its content into the web
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
