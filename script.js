7253ac218b7fee889cbda32fa0e818fcfdc4c2b8
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-03-21 17:27:31 -0300

    updateupdate

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-var div = document.createElement("div");
-div.style.width = "100px";
-div.style.height = "100px";
-div.style.background = "red";
-div.style.color = "white";
-div.innerHTML = "Hello";
+// var div = document.createElement("div");
+// div.style.width = "100px";
+// div.style.height = "100px";
+// div.style.background = "red";
+// div.style.color = "white";
+// div.innerHTML = "Hello";
 
+// document.body.appendChild(div);
+window.onload = createElement();
 
-document.body.appendChild(div);
+function createElement(cantidad){
+
+    var num = cantidad ;
+        for (let i = 0; i < num; i++) {
+            var div = document.createElement("div");
+            div.style.width = "100px";
+            div.style.height = "100px";
+            div.style.background = "red";
+            div.style.color = "white";
+            div.innerHTML = "Hello";
+
+            document.body.appendChild(div);
+        
+      }
+
+}
